34894741b8c8f7128645518275ed5934a6370b9b
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-05-10 00:49:48 +0530

    added image file upload function

--- SERVER/app.js
+++ SERVER/app.js
@@ -8,15 +8,11 @@ const app = express()
 dotenv.config()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use(cors())
 
 const port = process.env.PORT
-
-app.get('/', (req, res) => {
-    res.send('Router setup properly')
-})
-
 const uri = process.env.URI
 
 mongoose.connect(uri).then(() => {
@@ -26,29 +22,48 @@ mongoose.connect(uri).then(() => {
 })
 
 
+app.get('/', (req, res) => {
+    res.send('Router setup properly')
+})
 
 const formSchema = new mongoose.Schema({
     postTitle: { type: String, required: true },
     postContent: { type: String, required: true },
-    img: { data: Buffer, contentType: String }
+    formImg: { data: Buffer, contentType: String }
 })
 
 const Post = mongoose.model('Post', formSchema)
 
-const upload = multer({ storage: multer.memoryStorage() });
-app.post('/',upload.single('img'), async (req, res) => {
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        return cb(null, `${Date.now()}-${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage })
+
+app.post('/upload', upload.single('formImg'), async (req, res) => {
     try {
-        const { postTitle, postContent} = req.body
-        const img = {
-            data: req.file.buffer,
+        const { postTitle, postContent } = req.body
+        const formImg = {
+            data: req.file.path,
             contentType: req.file.mimetype
-          };
-        const post = new Post({ postTitle, postContent, img })
+        };
+        const post = new Post({ postTitle, postContent, formImg })
         await post.save()
         res.status(201).json({ message: 'Post Created.' })
+        
     } catch (err) {
         console.log(err)
     }
+})
+
+app.get('/upload', async (req, res) => {
+    const allImg = await Post.find()
+    res.status(200).json(allImg)
 })
 
 app.listen(port, () => {
